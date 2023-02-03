6c69f523fb772b4ae176b05eed4b8afd7d1a3ac4
Rowan <rowanmoss04@gmail.com>
2023-02-03 14:39:58 +0000

    Week 1 Exercise 4 - average of two entered numbers

---
+++ average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class average {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double num1;
+        double num2;
+        System.out.println("Enter number 1:");
+        num1 = scan.nextDouble();
+        System.out.println("Enter number 2:");
+        num2 = scan.nextDouble();
+        double average = (num1 + num2)/2;
+        System.out.println("The average is: " + average);
+    }
+}
