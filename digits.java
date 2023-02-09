2a55d6b6ca6bfe8a8ccd433c5af1acfb98bbce5c
Rowan <rowanmoss04@gmail.com>
2023-02-09 12:34:14 +0000

    Week 2 Exercise 3 - output using a loop

---
+++ digits.java
@@ -0,0 +1,10 @@
+public class digits {
+    public static void main(String[] args) {
+        System.out.println("Here are the digits from 0 to 9:");
+        int x = 0;
+        while (x<10) {
+            System.out.println(x);
+            x = x + 1;
+        }
+    }
+}
