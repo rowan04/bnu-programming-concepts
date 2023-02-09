0a110abaaf0da07c177f7791b0f45eb3c93b941d
Rowan <rowanmoss04@gmail.com>
2023-02-09 14:21:07 +0000

    Week 2 Exercise 4 - use loop to print seven times table

---
+++ timestable.java
@@ -0,0 +1,10 @@
+public class timestable {
+    public static void main(String[] args) {
+        System.out.println("Here is the seven times table:");
+        int count = 1;
+        while (count<=12) {
+            System.out.println(count + " x 7 = " + (count*7));
+            count = count + 1;
+        }
+    }
+}
