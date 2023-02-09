671f030a109ab63aa30e518ddd71df6357c7345f
Rowan <rowanmoss04@gmail.com>
2023-02-09 11:47:54 +0000

    Week 2 Exercise 1 - use if statement to output if user is adult or not
    - also makes use of toLowerCase() on user input
    - to ensure all forms of yes and no input are checked

--- age.java
+++ age.java
@@ -7,14 +7,21 @@ public class age {
         String birthdayThisYear;
         System.out.println("Please enter your age:");
         age = scan.nextInt();
+        if (age >= 18) {
+            System.out.println("You are an adult!");
+        } else {
+            System.out.println("You are a child.");
+        }
         System.out.println("Has it been your birthday this year?");
-        birthdayThisYear = scan.next();
-        if ("Yes".equals(birthdayThisYear)) {
+        birthdayThisYear = (scan.next().toLowerCase());
+        if ("yes".equals(birthdayThisYear)) {
             int birthyear = 2023 - age;
             System.out.println("You were born in " + birthyear);
-        } else {
+        } else if ("no".equals(birthdayThisYear)) {
             int birthyear = 2023 - age - 1;
             System.out.println("You were born in " + birthyear);
+        } else {
+            System.out.println("Invalid answer.");
         }
     }
 }
