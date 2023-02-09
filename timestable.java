aa57ef207f42bd3405537d8b49138d34c9fe1074
Rowan <rowanmoss04@gmail.com>
2023-02-09 14:44:09 +0000

    Week 2 Exercise 5 - allow user to pick times table

--- timestable.java
+++ timestable.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class timestable {
     public static void main(String[] args) {
-        System.out.println("Here is the seven times table:");
+        Scanner scan = new Scanner(System.in);
+        int num;
+        System.out.println("Which times table would you like to see? (Enter an integer):");
+        num = scan.nextInt();
+        System.out.println("Here is the " + num + " times table:");
         int count = 1;
         while (count<=12) {
-            System.out.println(count + " x 7 = " + (count*7));
+            System.out.println(count + " x " + num + " = " + (count*num));
             count = count + 1;
         }
     }
