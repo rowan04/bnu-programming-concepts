6bfaada3dac7ff45bd08708edb1a64c29e51ac14
Rowan <rowanmoss04@gmail.com>
2023-02-17 14:18:27 +0000

    Week 3 Exercise 1 - creating class

---
+++ Student.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Student {
+    public String name;
+    public int id;
+    
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+}
