fc138083198eedba1e7e3493719eafd7b3d07da1
Rowan <rowanmoss04@gmail.com>
2023-02-17 22:14:10 +0000

    Week 3 Exercise 7 - add constructor for second class and print method

--- Student.java
+++ Student.java
@@ -1,5 +1,51 @@
 import java.util.Scanner;
 
+class assign {
+    public static void main(String[] args) {
+        // instantiating object of student class
+        Student student1 = new Student("Rowan" ,22220407);
+        String name1 = student1.getName();
+        int id1 = student1.getId();
+        printStudent(name1, id1);
+        
+        // student constructor
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter student name:");
+        String studentName = scan.nextLine();
+        System.out.println("Enter student id:");
+        int studentId = scan.nextInt();
+
+        Student student = new Student(studentName, studentId);
+        String name = student.getName();
+        int id = student.getId();
+        printStudent(name, id);
+
+        // course constructor
+        System.out.println("Enter course code:");
+        String courseCode = scan.next();
+        System.out.println("Enter course name:");
+        String courseName = scan.next();
+        scan.close();
+
+        Course course = new Course(courseCode, courseName);
+        String course_code = course.getCode();
+        String course_name = course.getName();
+        printCourse(course_code, course_name);
+    }
+
+    //print student details
+    public static void printStudent(String name, int id) {
+        System.out.println("Student name is: " + name);
+        System.out.println("Student id is: " + id);
+    }
+
+    //print course details
+    public static void printCourse(String code, String name) {
+        System.out.println("Course code is: " + code);
+        System.out.println("Course name is: " + name);
+    }
+}
+
 public class Student {
     public String name;
     public int id;
@@ -15,33 +61,6 @@ public class Student {
     
     public int getId() {
         return id;
-    }
-}
-
-class assign {
-    public static void main(String[] args) {
-        // instantiating object of student class
-        Student student1 = new Student("Rowan" ,22220407);
-        String name1 = student1.getName();
-        int id1 = student1.getId();
-        printStudent(name1, id1);
-        
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter student name:");
-        String studentName = scan.nextLine();
-        System.out.println("Enter student id:");
-        int studentId = scan.nextInt();
-        scan.close();
-
-        Student student = new Student(studentName, studentId);
-        String name = student.getName();
-        int id = student.getId();
-        printStudent(name, id);
-    }
-
-    public static void printStudent(String name, int id) {
-        System.out.println("Student name is: " + name);
-        System.out.println("Student id is: " + id);
     }
 }
 
