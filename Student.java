a5dce283907639b1db1c6872097339e80684850f
Rowan <rowanmoss04@gmail.com>
2023-02-23 15:01:40 +0000

    Week 3 Extra - improving comments

--- Student.java
+++ Student.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 class assign {
     public static void main(String[] args) {
-        // instantiating object of student class
+        // instantiating object of student class using set values
         //Student student1 = new Student("Rowan" ,22220407);
         //String name1 = student1.getName();
         //int id1 = student1.getId();
         //printStudent(name1, id1);
         
-        // student constructor
+        // get values from user to instantiate object
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter student name:");
         String studentName = scan.nextLine();
@@ -20,16 +20,20 @@ class assign {
         String courseName = scan.next();
         scan.close();
 
+        // student constructor
         Student student = new Student(studentName, studentId);
         String name = student.getName();
         int id = student.getId();
 
+        // course constructor
         Course course = new Course(courseCode, courseName);
         String course_code = course.getCode();
         String course_name = course.getName();
         
+        // enroll student on course
         student.Enroll(course);
         
+        // print methods
         printStudent(name, id, course_name);
         printCourse(course_code, course_name);
     }
@@ -56,7 +60,6 @@ public class Student {
     public Student(String name, int id) {
         this.name = name;
         this.id = id;
-        //this.course = course;
     }
     
     public String getName() {
