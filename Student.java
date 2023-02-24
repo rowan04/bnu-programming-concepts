1f6e2abf75890cb6be608bd9cddfb85d0e4de3c8
Rowan <rowanmoss04@gmail.com>
2023-02-24 15:07:22 +0000

    Week 4 Exercise 2 - adding module objects to ArrayList and make it a course attribute

--- Student.java
+++ Student.java
@@ -1,12 +1,13 @@
 //import java.util.Scanner;
+import java.util.ArrayList;
 
 class assign {
     public static void main(String[] args) {
         // instantiating object of student class using set values
         String studentName = "Rowan";
         int studentId = 22220407;
-        String courseCode = "CO452";
-        String courseName = "Programming Concepts";
+        String courseCode = "BB1DTS2";
+        String courseName = "BSc (Hons) Digital and Technology Solutions";
         
         /* get values from user to instantiate object
         Scanner scan = new Scanner(System.in);
@@ -31,6 +32,38 @@ class assign {
         Course course = new Course(courseCode, courseName);
         String course_code = course.getCode();
         String course_name = course.getName();
+
+        // module constructors
+        String module1Name = "Programming Concepts";
+        String module1Code = "CO452";
+        Module module1 = new Module(module1Name, module1Code);
+        String module1_name = module1.getModuleName();
+        String module1_code = module1.getModuleCode();
+
+        String module2Name = "Computer Architectures";
+        String module2Code = "CO450";
+        Module module2 = new Module(module2Name, module2Code);
+        String module2_name = module2.getModuleName();
+        String module2_code = module2.getModuleCode();
+
+        String module3Name = "Web Development";
+        String module3Code = "CO456";
+        Module module3 = new Module(module3Name, module3Code);
+        String module3_name = module3.getModuleName();
+        String module3_code = module3.getModuleCode();
+
+        String module4Name = "Networking";
+        String module4Code = "CO451";
+        Module module4 = new Module(module4Name, module4Code);
+        String module4_name = module4.getModuleName();
+        String module4_code = module4.getModuleCode();
+
+        // add modules to ArrayList
+        ArrayList<Module> modules = new ArrayList<Module>();
+        modules.add(module1);
+        modules.add(module2);
+        modules.add(module3);
+        modules.add(module4);
         
         // enroll student on course
         student.Enroll(course);
@@ -80,6 +113,7 @@ public class Student {
 class Course {
     public String code;
     public String name;
+    public ArrayList<Module> modules;
     
     public Course(String code, String name) {
         this.code = code;
@@ -92,6 +126,10 @@ class Course {
     
     public String getName() {
         return name;
+    }
+
+    public void AddModules(ArrayList<Module> modules) {
+        this.modules = modules;
     }
 }
 
