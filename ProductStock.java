5917cef0e7faf2900d4be00a1ebfde3d2bca96f1
Rowan <rowanmoss04@gmail.com>
2023-03-03 10:24:10 +0000

    Week 5 Exercise 2 - fill out product class with attributes, get methods and print methods

--- ProductStock.java
+++ ProductStock.java
@@ -1,11 +1,45 @@
-class Main {
+class ProductStock {
     public static void main(String[] args) {
+        // product constructor
+        int id = 4081;
+        String name = "McLaren Cap";
+        int quantity = 300;
+        Product product = new Product(id, name, quantity);
+        int product_id = product.getId();
+        String product_name = product.getName();
+        int product_quantity = product.getQuantity();
+        printProduct(product_id, product_name, product_quantity);
+    }
 
+    public static void printProduct(int id, String name, int quantity) {
+        // output product details
+        System.out.println("Product name is: " + name + ", with id: " + id);
+        System.out.println("We have " + quantity + " of them.");
     }
 }
 
 class Product {
+    public int id;
+    public String name;
+    public int quantity;
 
+    public Product(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
 
 class StockList {
@@ -13,5 +47,5 @@ class StockList {
 }
 
 class StockDemo {
-    
+
 }
