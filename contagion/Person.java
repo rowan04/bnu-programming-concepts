7951aff2b3a09f9541219f91045bd09a5a5743b9
Rowan <rowanmoss04@gmail.com>
2023-03-10 12:40:37 +0000

    Week 6 - infecting

--- contagion/Person.java
+++ contagion/Person.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Person extends Actor
 {
+    private boolean isInfected = false;
+    
     /**
      * Create a person with a random initial direction of movement 
      */
@@ -22,9 +24,40 @@ public class Person extends Actor
      */
     public void act()
     {
+        move();
+        if (isInfected) {
+            infectOthers();
+        }
+    }
+    
+    /**
+     * Move the person randomly on screen.
+     */
+    private void move()
+    {
         move(2);
         if (isAtEdge()) {
             turn(Greenfoot.getRandomNumber(360));
+        }
+        if (Greenfoot.getRandomNumber(100) <20) {
+            turn(Greenfoot.getRandomNumber(81)-40);
+        }
+    }
+    
+    public void infect()
+    {
+        isInfected = true;
+        setImage("ppl3.png");
+    }
+    
+    /**
+     * Check if we are touching another person. If we are, infect them.
+     */
+    private void infectOthers()
+    {
+        Person other = (Person) getOneIntersectingObject(Person.class);
+        if (other != null) {
+            other.infect();
         }
     }
 }
